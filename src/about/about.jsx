5536b7775d459190019514abfdeaaede4c499755
GRUPOAMIL\pribeiro05 <pribeiro05@prestadores.amil.com.br>
2018-04-19 18:03:27 -0300

    frontend - inicio integração com back

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name="Sobre" small='Nós'></PageHeader>
+
+        <h2>Nossa História</h2>
+
+        <p>Mauris ornare malesuada mi a rhoncus. Suspendisse lacinia, mauris at feugiat tristique, lacus ante interdum mauris, quis feugiat nunc metus a ligula. Curabitur tincidunt malesuada aliquet. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla lobortis risus mattis turpis placerat posuere. Integer hendrerit lobortis risus quis egestas. Mauris sodales, odio a euismod cursus, massa nunc pharetra libero, vel molestie dui nunc id sapien.</p>
+        
+        <h2>Missão e Visão</h2>
+        <p>Praesent ut sollicitudin massa. Quisque eu cursus turpis, in lobortis erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam elit ac elit scelerisque tempus. Curabitur ipsum sapien, consectetur sed ornare at, ornare eu nunc. Nunc mollis ex purus, nec hendrerit ante volutpat sed. Morbi eget tortor accumsan, congue diam id, efficitur justo. Mauris posuere viverra molestie. Aenean ut ex quis nibh laoreet interdum id eu libero. Phasellus sodales sit amet urna eget lobortis. Cras vel sapien et velit bibendum pellentesque sed a mauris. Donec aliquet et ipsum sed tempor. Phasellus efficitur porttitor turpis in congue. Cras purus neque, congue et pharetra sit amet, egestas et metus. Suspendisse eget elementum lorem. Aliquam erat lectus, dapibus eget aliquet id, cursus id nisl.</p>
+
+        <h2>Imprensa</h2>
+        <p>Nulla facilisi. Maecenas ante neque, egestas eu consectetur a, sodales quis eros. Cras lacinia nisl in nisl dapibus condimentum in eu diam. Vivamus eget ligula justo. Proin aliquet purus ut odio malesuada posuere. Phasellus nunc felis, porttitor non risus sit amet, vehicula euismod orci. Vestibulum molestie, nulla a tincidunt consequat, mauris sapien venenatis dui, quis rhoncus risus tortor in metus. Nulla sit amet mollis leo. Etiam eget imperdiet elit, quis semper turpis.</p>
     </div>
 )
